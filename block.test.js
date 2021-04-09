b31c81f3eb4ffe47ab4d7d97b89810ac64dce982
Jerry Pan <Jerrypan2718@berkeley.edu>
2021-04-09 14:05:14 +0800

    init

--- block.test.js
+++ block.test.js
@@ -1,1 +1,19 @@
-describe('Block')
+describe('Block', () => {
+    const timestamp = 'a-date';
+    const lastHas = 'foo-hash';
+    const hash = 'bar-hash';
+    const data = ['blockchain', 'data'];
+    const block = new Block({ 
+        timestamp: timestamp,
+        lastHash: lastHash,
+        hash: hash,
+        data: data
+     });
+
+     it('has a timestamp, lashHash, hash, and data attribute', () => {
+         expect(block.timestamp).toEqual(timestamp);
+         expect(block.lastHash).toEqual(lastHash);
+         expect(block.hash).toEqual(hash);
+         expect(block.data).toEqual(data);
+     });
+});
